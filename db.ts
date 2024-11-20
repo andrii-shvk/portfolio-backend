29eccce860ccd99badfe98c68d35f74bb3fb8c99
Andrii Shevchuk <supamercron@gmail.com>
2024-11-19 19:18:02 -0500

    edit routes for EC2 server

--- db.ts
+++ db.ts
@@ -7,7 +7,7 @@ const client = new Pool({
         rejectUnauthorized: false,
     },
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_CLIENTPORT), // tunnel
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
