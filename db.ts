1a5951d7c53d1ed856a78466c44157e6faff6417
Andrii <andrii.shevk@gmail.com>
2024-12-07 18:05:06 -0500

    add docker

--- db.ts
+++ db.ts
@@ -6,8 +6,8 @@ const client = new Pool({
     ssl: {
         rejectUnauthorized: false,
     },
-    host: 'host.docker.internal',
-    port: Number(process.env.DB_CLIENTPORT), // tunnel
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
